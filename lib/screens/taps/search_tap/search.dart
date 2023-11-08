977ccd96a05a117329b7711f5a710dc4e38d4ec3
AtefMMO <mohamedatefimb@gmail.com>
2023-11-08 02:47:50 +0200

    recommended done(home) ~Atef

--- lib/screens/taps/search_tap/search.dart
+++ lib/screens/taps/search_tap/search.dart
@@ -19,78 +19,80 @@ class _SearchScreenState extends State<SearchScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        height: double.infinity,
-        color: AppTheme.black,
-        child: Column(
-          children: [
-            Container(), // Empty container can be removed
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: TextField(
-                onChanged: (value) {
-                  query = value;
-                  setState(() {});
-                },
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: AppTheme.darkGrey,
-                  // Background color of the search bar
-                  hintText: 'Search',
-                  hintStyle: TextStyle(color: AppTheme.lightGrey),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30.0),
+    return Container(
+      color: AppTheme.black,
+      child: SafeArea(
+        child: Container(
+          height: double.infinity,
+          child: Column(
+            children: [
+              Container(), // Empty container can be removed
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextField(
+                  onChanged: (value) {
+                    query = value;
+                    setState(() {});
+                  },
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: AppTheme.darkGrey,
+                    // Background color of the search bar
+                    hintText: 'Search',
+                    hintStyle: TextStyle(color: AppTheme.lightGrey),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: AppTheme.lighterGrey,
+                    ), // Search icon
                   ),
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: AppTheme.lighterGrey,
-                  ), // Search icon
                 ),
               ),
-            ),
-            (query.isEmpty)
-                ? Container(
-                    margin: EdgeInsets.only(
-                        top: MediaQuery.of(context).size.height * 0.27),
-                    child: Column(
-                      children: [
-                        Icon(Icons.local_movies,
-                            color: AppTheme.lighterGrey,
-                            size: MediaQuery.of(context).size.width * 0.35),
-                        Text(
-                          'No Movies Found',
-                          style: Theme.of(context).textTheme.titleLarge,
-                        )
-                      ],
-                    ),
-                  )
-                : FutureBuilder<SearchMoviesResponse>(
-              future: ApiManager.getSearchMovies(query),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(
-                    child: CircularProgressIndicator(
-                      color: AppTheme.gold,
-                    ),
-                  );
-                } else if (snapshot.hasError) {
-                  return Text(snapshot.error.toString());
-                } else {
-                  var searchResults = snapshot.data!.results ?? [];
-                  return Expanded(
-                    child: ListView.builder(
-                        itemBuilder: (context, index) {
-                          return MovieSearchItem(
-                              movie: searchResults[index]);
-                        },
-                        itemCount: searchResults.length),
-                  );
-                }
-              },
-            )
-
-          ],
+              (query.isEmpty)
+                  ? Container(
+                      margin: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.27),
+                      child: Column(
+                        children: [
+                          Icon(Icons.local_movies,
+                              color: AppTheme.lighterGrey,
+                              size: MediaQuery.of(context).size.width * 0.35),
+                          Text(
+                            'No Movies Found',
+                            style: Theme.of(context).textTheme.titleLarge,
+                          )
+                        ],
+                      ),
+                    )
+                  : FutureBuilder<SearchMoviesResponse>(
+                      future: ApiManager.getSearchMovies(query),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Center(
+                            child: CircularProgressIndicator(
+                              color: AppTheme.gold,
+                            ),
+                          );
+                        } else if (snapshot.hasError) {
+                          return Text(snapshot.error.toString());
+                        } else {
+                          var searchResults = snapshot.data!.results ?? [];
+                          return Expanded(
+                            child: ListView.builder(
+                                itemBuilder: (context, index) {
+                                  return MovieSearchItem(
+                                      movie: searchResults[index]);
+                                },
+                                itemCount: searchResults.length),
+                          );
+                        }
+                      },
+                    )
+            ],
+          ),
         ),
       ),
     );
